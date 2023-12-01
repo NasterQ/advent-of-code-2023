03d07690fe045df122bfb2066249889a540341e3
Filip Maćkowiak <Fil.Mackowiak@gmail.com>
2023-12-01 18:00:36 +0100

    D1P2: Part 2 init

---
+++ day-1/part2-test-data.js
@@ -0,0 +1,9 @@
+const testData = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+exports.testData = testData;
---
+++ day-1/part2.js
@@ -0,0 +1,9 @@
+const { testData } = require("./part2-test-data");
+
+function findNumbers(text) {
+  const lines = text.split("\n");
+}
+
+const testResult = findNumbers(testData);
+console.log(testResult);
+
